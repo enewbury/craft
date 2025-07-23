bc0a95a3bc103065bc1fb3817ccd4cae147d5b6f
Eric Newbury <enewbury@users.noreply.github.com>
2025-07-23 15:12:53 -0600

    should be a default shm_path

--- config/config.exs
+++ config/config.exs
@@ -23,6 +23,4 @@ if config_env() == :test do
   config :craft, :logger, [{:handler, :nexus_handler, Craft.Nexus, %{}}]
 end
 
-config :craft, :clock_bound_shm_path, "/var/run/clockbound/shm0"
-
 #import_config "#{config_env()}.exs"
